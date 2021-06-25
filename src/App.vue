ee3a4133c7d9bb4ae3988e56851177cb961854ed
DestCom <azevedo-a@hotmail.fr>
2021-06-25 22:29:38 +0200

    v1.5

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,24 @@
 <template>
-<div>
-  <div class="bloc1"></div>
-  <div class="bloc2"></div>
-  <div class="bloc3"></div>
-  <div class="bloc4"></div>
+  <div>
+    <div class="bloc1"></div>
+    <div class="bloc2"></div>
+    <div class="bloc3"></div>
+    <div class="bloc4"></div>
 
-  <div class="text">
-    <label>Enter a date in the 'month/day' format : </label>
-    <input type="text" v-model="dates" v-on:keyup.enter="test" />
+    <div class="text">
+      <label>Enter a date in the 'month/day' format separated by commas : </label>
+      <input type="text" v-model="dates" v-on:keyup.enter="test" />
+    </div>
+    <div class="title">
+      <h1>Your dates in the history</h1>
+      <p>You want to explore {{numberOfDates}} dates</p>
+    </div>
     <div class="facts">
       <div v-for="date in tab" :key="date">
-        <Display :datesGiven="date"  />
+        <Display :datesGiven="date" />
       </div>
     </div>
-  </div> 
-</div>
+  </div>
 </template>
 
 <script>
@@ -32,7 +36,9 @@ export default{
     }
   },
   computed:{
-
+    numberOfDates: function(){
+      return this.tab.length
+    }
   },
   methods:{
     test: function(){
@@ -46,7 +52,8 @@ export default{
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap');
 
-html,body{
+html,
+body {
   margin: 0;
   padding: 0;
   width: 100%;
@@ -56,7 +63,7 @@ html,body{
   overflow: hidden;
 }
 
-.bloc1{
+.bloc1 {
   position: absolute;
   top: 10%;
   left: -1%;
@@ -68,7 +75,7 @@ html,body{
   border-left: 3000px solid transparent;
 }
 
-.bloc2{
+.bloc2 {
   position: absolute;
   top: 0%;
   width: 100%;
@@ -78,7 +85,7 @@ html,body{
   background-color: darkblue;
 }
 
-.bloc3{
+.bloc3 {
   position: absolute;
   top: 20%;
   left: 25vw;
@@ -91,7 +98,7 @@ html,body{
   background-image: linear-gradient(to right bottom, cyan 0%, cyan 50%, transparent 50%);
 }
 
-.bloc4{
+.bloc4 {
   position: absolute;
   left: 25vw;
   width: 50vw;
@@ -101,11 +108,12 @@ html,body{
   background-color: cyan;
 }
 
-.text{
+.text {
+  padding-top: 3%;
   margin-right: auto;
   margin-left: auto;
   position: relative;
-  z-index: 5;
+  z-index: 4;
   top: 10%;
   left: 0vw;
   width: 25vw;
@@ -118,17 +126,68 @@ input[type=text] {
   border: none;
   border-bottom: 2px solid black;
   background-color: cyan;
+  text-align: center;
 }
 
 input[type=text]:focus {
   outline-width: 0;
-  
+  border-bottom: 2px solid white;
+
 }
 
-.facts{
+.facts {
+  position: relative;
+  margin-right: auto;
+  margin-left: auto;
+  height: 50vh;
+  overflow-y: auto;
+  width: 50vw;
+  z-index: 5;
+}
+
+.facts>div {
+  text-align: left;
+  background-color: white;
+  border-radius: 25px;
   margin-top: 10%;
-  height: 90vh;
-  overflow-y: scroll;
+  padding: 5px;
+  height: 25vh;
   width: 30vw;
+  margin-right: auto;
+  margin-left: auto;
+}
+
+.title{
+  position: relative;
+  background-color: black;
+  color: white;
+  z-index: 7 !important;
+  width: 35vw;
+  min-height: 20vh;
+  margin-top: 2%;
+  padding-top: 3%;
+  text-align: center;
+  margin-right: auto;
+  margin-left: auto;
+}
+
+.facts::-webkit-scrollbar-track
+{
+	-webkit-box-shadow: inset 0 0 6px transparent;
+	border-radius: 10px;
+	background-color: transparent;
+}
+
+.facts::-webkit-scrollbar
+{
+	width: 12px;
+	background-color: transparent;
+}
+
+.facts::-webkit-scrollbar-thumb
+{
+	border-radius: 10px;
+	-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
+	background-color: cyan;
 }
 </style>
--- src/components/Display.vue
+++ src/components/Display.vue
@@ -1,62 +1,60 @@
 <template>
-<div>
     <h2>{{date}}/{{month}}</h2>
     <p>{{msg}}</p>
-</div>
 </template>
 
 <script>
 const axios = require('axios');
 
-export default{
-  name:'Display',
-  props:{
-      datesGiven: String
-  },
-  data(){
-    return{
-     msg: '',
-     date:'',
-     month:''
-    }
-  },
-  mounted (){
-      this.date = this.datesGiven.substr(0, this.datesGiven.indexOf('/'))
-      this.month = this.datesGiven.substr(this.datesGiven.indexOf('/')+1, this.datesGiven.length)
-      if(!(parseInt(this.date)>31)){
-          if(!(parseInt(this.month)>12)){
-            let url ='http://www.numbersapi.com/' + this.date + '/' + this.month + '/date';
-            axios
-                .get(url)
-                .then((response) => {
-                    this.msg = response.data
-                })
-                .catch(function (error){
-                    this.msg = 'An error as occured' + error
-                })
+export default {
+    name: 'Display',
+    props: {
+        datesGiven: String
+    },
+    data() {
+        return {
+            msg: '',
+            date: '',
+            month: ''
+        }
+    },
+    mounted() {
+        this.month = this.datesGiven.substr(0, this.datesGiven.indexOf('/'))
+        this.date = this.datesGiven.substr(this.datesGiven.indexOf('/') + 1, this.datesGiven.length)
+        console.log(this.date, this.month)
+        if (!(parseInt(this.date) > 31)) {
+            if (!(parseInt(this.month) > 12)) {
+                if (!(parseInt(this.date) < 1)) {
+                    if (!(parseInt(this.month) < 1)) {
+                        let url = 'http://www.numbersapi.com/' + this.month + '/' + this.date + '/date';
+                        console.log(url)
+                        axios
+                            .get(url)
+                            .then((response) => {
+                                this.msg = response.data
+                            })
+                            .catch(function (error) {
+                                this.msg = 'An error as occured' + error
+                            })
+                    } else {
+                        this.msg = 'Please enter a correct date';
+                    }
+                } else {
+                    this.msg = 'Please enter a correct date';
+                }
+            } else {
+                this.msg = 'Please enter a correct date';
             }
-            else{
+        } else {
             this.msg = 'Please enter a correct date';
         }
     }
-    else{
-        this.msg = 'Please enter a correct date';
-    }
-  }
 }
 
 </script>
 
 <style scoped>
-    div{
-        text-align: left;
-        background-color: white;
-        border-radius: 25px;
-        margin-top: 10%;
-        padding: 5px;
-        height: 20vh;
-        width: 25vw;
-    }
+    
 
     h2{
         margin-top: 5px;
